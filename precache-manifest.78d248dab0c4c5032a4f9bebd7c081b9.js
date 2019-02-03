91814a95b16f3413e8fadb6a1e92acd20c1c2528
Vinicius Cechin <vinicechin@gmail.com>
2019-02-03 11:14:08 -0200

    Updates

---
+++ precache-manifest.78d248dab0c4c5032a4f9bebd7c081b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vini-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a56acc85a91b5bb978b0",
+    "url": "/vini-page/static/js/runtime~main.a56acc85.js"
+  },
+  {
+    "revision": "eb94f853404916fb0d77",
+    "url": "/vini-page/static/js/main.eb94f853.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/vini-page/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "eb94f853404916fb0d77",
+    "url": "/vini-page/static/css/main.22b830f4.chunk.css"
+  },
+  {
+    "revision": "15c5aa6742545ee6c0ee53516d6f2566",
+    "url": "/vini-page/index.html"
+  }
+];
